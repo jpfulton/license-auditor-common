8bd825256b7f494d7e3bacc16bc103e86cbfdbd0
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-10-13 12:09:54 -0500

    Remove calling package version function.

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,15 +3,11 @@ import { removeDuplicates } from "./duplicate-remover";
 import { mapLicenseByGroupings } from "./map-license-by-groupings";
 import { getLicensesMarkdown } from "./markdown-helpers";
 import { DependencyOutputter, MetadataOutputter } from "./outputters";
-import {
-  getCallingProjectVersionString,
-  getCurrentVersionString,
-} from "./root-project";
+import { getCurrentVersionString } from "./root-project";
 
 export type { DependencyOutputter, MetadataOutputter };
 
 export {
-  getCallingProjectVersionString,
   getConfiguration,
   getConfigurationFromUrl,
   getCurrentVersionString,
