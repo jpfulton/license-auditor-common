93c1a77bae5b47e5f51313e3b369fbab5ff9f48e
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-10-12 20:38:10 -0500

    Add function get version of calling project.

--- src/util/root-project.ts
+++ src/util/root-project.ts
@@ -1,7 +1,11 @@
 import { readFileSync } from "fs";
 
-// return the version string from this module's package.json
-export function getCurrentVersionString() {
+/**
+ * Returns the current version string of the package.
+ * If the version cannot be determined, returns "UNKNOWN".
+ * @returns {string} The current version string of the package.
+ */
+export const getCurrentVersionString = (): string => {
   try {
     const packageJson = JSON.parse(
       readFileSync(`${__dirname}/../../../package.json`).toString()
@@ -13,4 +17,23 @@ export function getCurrentVersionString() {
     // which have a different path relationship to the package.json file
     return "UNKNOWN";
   }
-}
+};
+
+/**
+ * Returns the current version string of the calling project.
+ * If the version cannot be determined, returns "UNKNOWN".
+ * @returns {string} The current version string of the calling project.
+ */
+export const getCallingProjectVersionString = (): string => {
+  try {
+    const packageJson = JSON.parse(
+      readFileSync(`${__dirname}/../../../../../package.json`).toString()
+    );
+    const version = packageJson?.version ?? "UNKNOWN";
+    return version;
+  } catch (e) {
+    // this error happens when running the tests
+    // which have a different path relationship to the package.json file
+    return "UNKNOWN";
+  }
+};
